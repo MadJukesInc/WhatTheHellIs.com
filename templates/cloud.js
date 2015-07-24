df5d68495dbe99dc4274d005c1bf59b8c03d0dba
Jason Wuertz [Jason Würtz] <jwuertz@upei.ca>
2015-07-24 15:05:27 -0300

    feat(index): add button listeners
    add call to REST

--- templates/cloud.js
+++ templates/cloud.js
@@ -10,9 +10,31 @@ $(document).ready(function(e){
         /* ... */
     ];
 
+    generateCloud(words);
+});
+
+$.fn.api.settings.api = {
+    'submit query': '/api/v1/{query}/wc',
+};
+
+var submitQuery = function(e){
+    const query = $('input').val().replaceAll(' ', '%20');
+    $.get({
+        url: 'localhost:5000/api/v1/'+query+'/wc',
+        success: processData(res),
+        dataType: 'json',
+    })
+};
+
+var processData = function(res){
+    generateCloud(res.counts[0]);
+    $('#summary').innerText(res.summaries[0]);
+};
+
+var generateCloud = function(words){
     const $cloud = $('#wordCloud');
     $cloud.jQCloud(words, {
         height: $cloud.parent().innerWidth()/2,
         width: $cloud.parent().innerWidth()-100,
     });
-});
+}
