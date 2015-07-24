6b02ca1fb95c3cc3053f50dfb6dba9b2fb05abb8
pineauk <kpineau@upei.ca>
2015-07-24 11:05:41 -0300

    feat(main.py): add endpoints for count and np counts

--- server/controllers/main.py
+++ server/controllers/main.py
@@ -2,15 +2,20 @@ from flask import Blueprint, render_template, flash, request, redirect, url_for,
 from flask.ext.login import login_user, logout_user, login_required
 
 from server.extensions import cache
+from server.frequency import get_frequencies
+from server.frequency import get_noun_frequencies
 
 static_results = {}
 static_results['thing'] = u'This is the example result'
 static_results['kyle'] =  u'The Efficiency Nazi'
 static_results['nikita'] = u'The Git Nazi'
 static_results['jason'] = u'The Organization Nazi'
+static_results['dave'] = u'The Grammar Nazi'
 
 main = Blueprint('main', __name__)
 api_namespace = '/api/v1'
+
+
 
 @main.route('/')
 @cache.cached(timeout=1000)
@@ -21,3 +26,17 @@ def home():
 def query(search):
     print search
     return  jsonify(query = search , description = static_results[search])
+
+@main.route(api_namespace+'/counts/<search>')
+def w_counts(search):
+    return jsonify(
+    query = search ,
+    description = static_results[search],
+    counts=get_noun_frequencies(static_results[search]))
+
+@main.route(api_namespace+'/np_counts/<search>')
+def np_counts(search):
+    return jsonify(
+    query = search ,
+    description = static_results[search],
+    counts=get_frequencies(static_results[search]))
