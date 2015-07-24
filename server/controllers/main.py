ed6db58ceaa9e47b0ab3412367f02c16b8784825
pineauk <kpineau@upei.ca>
2015-07-24 10:43:25 -0300

    feat(main.py): add more examples to teh static dict

--- server/controllers/main.py
+++ server/controllers/main.py
@@ -1,13 +1,13 @@
-from flask import Blueprint, render_template, flash, request, redirect, url_for
+from flask import Blueprint, render_template, flash, request, redirect, url_for, jsonify
 from flask.ext.login import login_user, logout_user, login_required
 
 from server.extensions import cache
 
 static_results = {}
-static_results['thing'] = {
-    'name': 'thing',
-    'description': 'This is the example result'
-}
+static_results['thing'] = u'This is the example result'
+static_results['kyle'] =  u'The Efficiency Nazi'
+static_results['nikita'] = u'The Git Nazi'
+static_results['jason'] = u'The Organization Nazi'
 
 main = Blueprint('main', __name__)
 api_namespace = '/api/v1'
