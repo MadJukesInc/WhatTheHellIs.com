29429eb4bd124f6965a9e8708f67a70e568d0b89
Jason Wuertz [Jason Würtz] <jwuertz@upei.ca>
2015-07-24 15:50:37 -0300

    feat(cloud): submit on enter

--- server/templates/cloud.js
+++ server/templates/cloud.js
@@ -1,21 +1,27 @@
-$(document).ready(function(e){
-  var words = [
-  {text: "Lorem", weight: 13},
-  {text: "Ipsum", weight: 10.5},
-  {text: "Dolor", weight: 9.4},
-  {text: "Sit", weight: 8},
-  {text: "Amet", weight: 6.2},
-  {text: "Consectetur", weight: 5},
-  {text: "Adipiscing", weight: 5},
-  /* ... */
-  ];
+$(document).ready(function(e) {
+    var words = [
+        {text: "Lorem", weight: 13},
+        {text: "Ipsum", weight: 10.5},
+        {text: "Dolor", weight: 9.4},
+        {text: "Sit", weight: 8},
+        {text: "Amet", weight: 6.2},
+        {text: "Consectetur", weight: 5},
+        {text: "Adipiscing", weight: 5},
+        /* ... */
+    ];
 
-  const $cloud = $('#wordCloud');
+    const $cloud = $('#wordCloud');
 
-  $cloud.jQCloud(words, {
-      height: $cloud.parent().innerWidth()/2,
-      width: $cloud.parent().innerWidth()-100,
-  });
+    $cloud.jQCloud(words, {
+        height: $cloud.parent().innerWidth() / 2,
+        width: $cloud.parent().innerWidth() - 100,
+    });
+};
+
+$('input').keyup(function(e){
+   if(e.keyCode == 13){
+       submitQuery();
+   };
 });
 
 var processData = function(res){
@@ -29,9 +35,9 @@ var submitQuery = function (e) {
     const query = $('input').val().trim().replace(' ', '%20');
     $.get('http://127.0.0.1:5000/api/v1/wiki/' + query + '/wc', processData);
 };
+
 var generateCloud = function(words){
     const $cloud = $('#wordCloud');
-
     $cloud.jQCloud('update', words);
 };
 
