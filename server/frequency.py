5496d53e92613072f343f4fc732f75af3ca67f5f
pineauk <kpineau@upei.ca>
2015-07-24 11:05:09 -0300

    featu(frequency): add function to get word counts and noune counts

--- server/frequency.py
+++ server/frequency.py
@@ -1,5 +1,9 @@
 from textblob import TextBlob
 
-def get_frequencies(text):
-    blob = textblob(text)
+def get_noun_frequencies(text):
+    blob = TextBlob(text)
     return blob.np_counts
+
+def get_frequencies(text):
+    blob = TextBlob(text)
+    return blob.word_counts
