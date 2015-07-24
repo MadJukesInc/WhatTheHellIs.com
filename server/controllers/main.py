2954d8adc1f262d5a3a56422b05d30ef494e0a90
pineauk <kpineau@upei.ca>
2015-07-24 10:24:09 -0300

    feat(main.py): add the static result dict

--- server/controllers/main.py
+++ server/controllers/main.py
@@ -3,6 +3,12 @@ from flask.ext.login import login_user, logout_user, login_required
 
 from server.extensions import cache
 
+static_results = []
+static_results['thing'] = {
+    'name': 'thing',
+    'description': 'This is the example result'
+}
+
 main = Blueprint('main', __name__)
 api_namespace = '/api/v1'
 
@@ -11,9 +17,6 @@ api_namespace = '/api/v1'
 def home():
     return render_template('index.html')
 
-@main.route(api_namespace+'/query/:param')
-def query():
-    return {
-        'description': 'The thing',
-        'name': 'thing'
-    }
+@main.route(api_namespace+'/query/<search>')
+def query(search):
+    return static_results[search]
